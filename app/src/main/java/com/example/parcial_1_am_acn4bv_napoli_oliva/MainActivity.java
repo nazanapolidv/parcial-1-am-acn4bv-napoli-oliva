d74feac27b4b5449e8211603f01b7595e5c1a755
gusoliva85 <gusoliva85@gmail.com>
2025-10-04 19:12:02 -0300

    Cambio de Peliculas y HTML

--- app/src/main/java/com/example/parcial_1_am_acn4bv_napoli_oliva/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_napoli_oliva/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         peliculas.add(new Pelicula(3, "Ready Player One", 2018, "Ciencia Ficción", R.drawable.Ready_Player));
         peliculas.add(new Pelicula(6, "Matrix", 1999, "Ciencia Ficción", R.drawable.Matrix));
         peliculas.add(new Pelicula(1, "Kill Bill", 2003, "Acción", R.drawable.Kill_Bill));
-        peliculas.add(new Pelicula(4, "Scott Pilgrim vs the World", 2010, "Comedia / Acción", R.drawable.Scott_Pilgrim));
+        peliculas.add(new Pelicula(4, "Scott Pilgrim vs the World", 2010, "Comedia / Acción", R.drawable.scott_pilgrim));
         peliculas.add(new Pelicula(5, "Duro de Matar 4", 2007, "Acción", R.drawable.Duro_De_Matar));
         peliculas.add(new Pelicula(9, "La Naranja Mecánica", 1971, "Drama / Distopía", R.drawable.Naranja));
         peliculas.add(new Pelicula(8, "Shingeki no Kyoshin: The Last Attack", 2023, "Anime / Acción", R.drawable.Shingeki));
