2a3bb4d8449294469ee2653f451a2c9bbc7ac5d1
gusoliva85 <gusoliva85@gmail.com>
2025-10-04 19:49:02 -0300

    Cambio de Peliculas y HTML

--- app/src/main/java/com/example/parcial_1_am_acn4bv_napoli_oliva/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_napoli_oliva/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.parcial_1_am_acn4bv_napoli_oliva;
 
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
@@ -16,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
 
         peliculas = new ArrayList<>();
 
-        peliculas.add(new Pelicula(2, "Batman Begins", 2005, "Acción", R.drawable.Batman_Begin));
-        peliculas.add(new Pelicula(3, "Ready Player One", 2018, "Ciencia Ficción", R.drawable.Ready_Player));
-        peliculas.add(new Pelicula(6, "Matrix", 1999, "Ciencia Ficción", R.drawable.Matrix));
-        peliculas.add(new Pelicula(1, "Kill Bill", 2003, "Acción", R.drawable.Kill_Bill));
-        peliculas.add(new Pelicula(4, "Scott Pilgrim vs the World", 2010, "Comedia / Acción", R.drawable.scott_pilgrim));
-        peliculas.add(new Pelicula(5, "Duro de Matar 4", 2007, "Acción", R.drawable.Duro_De_Matar));
-        peliculas.add(new Pelicula(9, "La Naranja Mecánica", 1971, "Drama / Distopía", R.drawable.Naranja));
-        peliculas.add(new Pelicula(8, "Shingeki no Kyoshin: The Last Attack", 2023, "Anime / Acción", R.drawable.Shingeki));
-        peliculas.add(new Pelicula(7, "Akira", 1988, "Animación / Ciencia Ficción", R.drawable.Akira));
+        peliculas.add(new Pelicula(2, "Batman Begins", 2005, "Acción", R.drawable.batmanbegin));
+        peliculas.add(new Pelicula(3, "Ready Player One", 2018, "Ciencia Ficción", R.drawable.readyplayer));
+        peliculas.add(new Pelicula(6, "Matrix", 1999, "Ciencia Ficción", R.drawable.matrix));
+        peliculas.add(new Pelicula(1, "Kill Bill", 2003, "Acción", R.drawable.killbill));
+        peliculas.add(new Pelicula(4, "Scott Pilgrim vs the World", 2010, "Comedia / Acción", R.drawable.scottpilgrim));
+        peliculas.add(new Pelicula(5, "Duro de Matar 4", 2007, "Acción", R.drawable.durodematar));
+        peliculas.add(new Pelicula(9, "La Naranja Mecánica", 1971, "Drama / Distopía", R.drawable.naranja));
+        peliculas.add(new Pelicula(8, "Shingeki no Kyoshin: The Last Attack", 2023, "Anime / Acción", R.drawable.shingeki));
+        peliculas.add(new Pelicula(7, "Akira", 1988, "Animación / Ciencia Ficción", R.drawable.akira));
 
     }
 
