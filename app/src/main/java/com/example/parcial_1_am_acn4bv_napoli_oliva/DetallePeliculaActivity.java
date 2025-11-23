0de2f1ea3937491706aa146a58b41534f0288735
gusoliva85 <gusoliva85@gmail.com>
2025-11-22 21:03:22 -0300

    Segundo activity. de detalle

---
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_napoli_oliva/DetallePeliculaActivity.java
@@ -0,0 +1,49 @@
+package com.example.parcial_1_am_acn4bv_napoli_oliva;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetallePeliculaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_pelicula);
+
+        // Recibir los datos
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            String titulo = extras.getString("EXTRA_TITULO");
+            int anio = extras.getInt("EXTRA_ANIO");
+            String genero = extras.getString("EXTRA_GENERO");
+            int imagenId = extras.getInt("EXTRA_IMAMGEN");
+            String descripcionCorta = extras.getString("EXTRA_DESCRIPCION_CORTA");
+
+            // Obtener referencias
+            TextView txtTitulo = findViewById(R.id.detalleTitulo);
+            TextView txtDatos = findViewById(R.id.detalleDatos);
+            TextView txtDescripcion = findViewById(R.id.detalleDescripcion);
+            ImageView imgPoster = findViewById(R.id.detalleImagen);
+            Button btnVolver = findViewById(R.id.btnVolver);
+
+            // Asignar valores
+            txtTitulo.setText(titulo);
+            txtDatos.setText(genero + " (" + anio + ")");
+            txtDescripcion.setText(descripcionCorta);
+            imgPoster.setImageResource(imagenId);
+
+            btnVolver.setOnClickListener(v -> {
+                finish(); // cierra Activity y vuelve a la anterior (MainActivity)
+            });
+        }
+
+    }
+}
